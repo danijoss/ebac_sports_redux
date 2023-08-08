10559356eebafa39158310eefc7bf5bea07ba83f
Daniel <dan.henrri@gmail.com>
2023-08-07 21:33:35 -0300

    feat: query

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,14 +1,19 @@
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
 
 import * as S from './styles'
 
 type Props = {
-  produtos: ProdutoType[]
+  //   produtos: ProdutoType[]
   favoritos: ProdutoType[]
 }
 
-const ProdutosComponent = ({ produtos, favoritos }: Props) => {
+const ProdutosComponent = ({ favoritos }: Props) => {
+  const { data: produtos, isLoading } = useGetProdutosQuery()
+
+  if (isLoading) return <h2>Carregando...</h2>
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
     const IdsDosFavoritos = favoritos.map((f) => f.id)
@@ -19,7 +24,7 @@ const ProdutosComponent = ({ produtos, favoritos }: Props) => {
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
